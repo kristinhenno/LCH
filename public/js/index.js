c149504957c976e3b79d3fd1217be863006bded1
Kristin Henno <kristinhenno@gmail.com>
2017-11-08 23:24:46 -0800

    lolos port done

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("button");
 var d3 = document.getElementById("d3");
 var count = 0;
+var lolo = ["lolo1","lolo2", "lolo3", "lolo4", "lolo5", "lolo6", "lolo7", "lolo8", "lolo9", "lolo10", "lolo11", "lolo12", "lolo13"];
 
 function openNav() {
     button.style.display = "none";
@@ -25,9 +26,17 @@ function images2(){
 
 }
 
-
 $(document).ready(function() {
+
     setTimeout(images, 3000);
+
+    $("#lolosbungalow").on("click", function(event){
+    	console.log("poop");
+    	$("#projects").empty();
+    	for (i=0; i<lolo.length; i++){
+    	$("#projects").append("<div class='imgcontainer1'><img id='lolo" +  [i] + "' class='allpics' src='/public/assets/images/Lolo/" + lolo[i]+ ".jpg'/></div>");
+    }
+    })
 
 });
 
