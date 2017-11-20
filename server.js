f8cdffc25eb2f1f57569df55adc7b73fd6d2bc5c
Kristin Henno <kristinhenno@gmail.com>
2017-11-19 19:48:36 -0800

    git init

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var fs = require("fs");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 var cheerio = require("cheerio");
 var request = require("request");
 
