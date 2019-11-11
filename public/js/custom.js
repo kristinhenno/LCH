a5bfcba9d285247fbfb7d67852cc7a42921afc23
Kristin Henno <kristinhenno@gmail.com>
2019-11-10 17:42:44 -0800

    new pics

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,18 +1,15 @@
-$(document).ready(function(){
-	var userFeed = new Instafeed({
-		get: 'user',
-		userId: '1696879779',
-		limit:30,
-		resolution: 'standard_resolution',
-		accessToken: '1696879779.1677ed0.1646f16d5bee48309c584313863c9935',
-		sortBy: 'most-recent',
-		orientation: 'square',
-		template: '<div class="gallery"><div class="instafeed-image-square"><a href="{{link}}" target="_blank"><img class="instaimg" src="{{image}}" alt="{{caption}} class="img-fluid"/></a></div></div>',
-
-	});
-	userFeed.run();
-})
-
-
-
-
+$(document).ready(function() {
+  var userFeed = new Instafeed({
+    get: "user",
+    userId: "1696879779",
+    limit: 30,
+    resolution: "standard_resolution",
+    accessToken:
+      "1696879779.1677ed0.f3471eb02120436c9fb861669842884b",
+    sortBy: "most-recent",
+    orientation: "square",
+    template:
+      '<div class="gallery"><div class="instafeed-image-square"><a href="{{link}}" target="_blank"><img class="instaimg" src="{{image}}" alt="{{caption}} class="img-fluid"/></a></div></div>'
+  });
+  userFeed.run();
+});
--- public/js/maison.js
+++ public/js/maison.js
@@ -1,58 +1,90 @@
 var button = document.getElementById("button");
 var d3 = document.getElementById("d3");
-var maison = ["2", "1", "4", "3", "5", "6", "8", "9", "10", "11", "12",
- "13", "14", "15", "16", "19", "21", "22", "24", 
- "26", "27", "28", "29", "30", "31", "32", "33"];
+var maison = [
+  "2",
+  "1",
+  "4",
+  "3",
+  "5",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "19",
+  "21",
+  "22",
+  "24",
+  "26",
+  "28",
+  "28.1",
+  "28.2",
+  "29",
+  "30",
+
+  "32",
+  "33"
+];
 
 function openNav() {
-    button.style.display = "none";
-    d3.style.display = "block";
+  button.style.display = "none";
+  d3.style.display = "block";
 }
 
 function closeNav() {
-    button.style.display = "inline-block";
-    d3.style.display = "none";
+  button.style.display = "inline-block";
+  d3.style.display = "none";
 }
 
 function lightbox(idx) {
-    //Show the slider wrapper
-    var ninjaSldr = document.getElementById("ninja-slider");
-    ninjaSldr.parentNode.style.display = "block";
+  //Show the slider wrapper
+  var ninjaSldr = document.getElementById("ninja-slider");
+  ninjaSldr.parentNode.style.display = "block";
 
-    //Then init the slider
-    //Note: The { initSliderByCallingInitFunc: true } option in the 
-    // ninja-slider.js tells the page not to initiate the slider
-    // unless the following init(idx) function is called.
-    nslider.init(idx);
+  //Then init the slider
+  //Note: The { initSliderByCallingInitFunc: true } option in the
+  // ninja-slider.js tells the page not to initiate the slider
+  // unless the following init(idx) function is called.
+  nslider.init(idx);
 
-    //Then mimic clicking the fullscreen button to popup the modal
-    var fsBtn = document.getElementById("fsBtn");
-    fsBtn.click();
+  //Then mimic clicking the fullscreen button to popup the modal
+  var fsBtn = document.getElementById("fsBtn");
+  fsBtn.click();
 }
 
 function fsIconClick(isFullscreen) {
-    //Note: fsIconClick is the default event handler of the fullscreen button
-    if (isFullscreen) {
-        var ninjaSldr = document.getElementById("ninja-slider");
-        ninjaSldr.parentNode.style.display = "none";
-    }
+  //Note: fsIconClick is the default event handler of the fullscreen button
+  if (isFullscreen) {
+    var ninjaSldr = document.getElementById("ninja-slider");
+    ninjaSldr.parentNode.style.display = "none";
+  }
 }
-
-
-
-
 
 $(document).ready(function() {
+  for (i = 0; i < maison.length; i++) {
+    $("#projects").append(
+      "<div id='imgcontainer" +
+        [i] +
+        "'><img onclick='lightbox(" +
+        [i] +
+        ")' id='k" +
+        [i] +
+        "' class='maison' src='/assets/images/Maison/" +
+        maison[i] +
+        ".jpg'/><div id='myModal' class='modal'> <span class = 'close'>&times;</span><img class='modal-content' id='img01'></div></div>"
+    );
+  }
+
+  if ($(window).width() > 769) {
     for (i = 0; i < maison.length; i++) {
-        $("#projects").append("<div id='imgcontainer" + [i] + "'><img onclick='lightbox(" + [i] + ")' id='k" + [i] + "' class='maison' src='/assets/images/Maison/" + maison[i] + ".jpg'/><div id='myModal' class='modal'> <span class = 'close'>&times;</span><img class='modal-content' id='img01'></div></div>");
-
+      $("#ninja").append(
+        "<li><a class='ns-img' href='/assets/images/Maison/" +
+          maison[i] +
+          ".jpg'></a></li>"
+      );
     }
-
- if ($(window).width() > 769) {
-    for (i = 0; i < maison.length; i++) {
-        $("#ninja").append("<li><a class='ns-img' href='/assets/images/Maison/" + maison[i] + ".jpg'></a></li>")
-
-    }
-}
-
-})
+  }
+});
